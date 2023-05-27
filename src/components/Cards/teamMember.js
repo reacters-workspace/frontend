a0815654c8b3010e6b89ac7ead3237485444772c
AnasNemrawi <anasnemrawi3000@gmail.com>
2023-05-27 13:42:49 +0000

    updating the team members section

--- src/components/Cards/teamMember.js
+++ src/components/Cards/teamMember.js
@@ -1,5 +1,8 @@
+
 import React from 'react';
 import './teamMember.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 
 export default function TeamMember() {
     return (
@@ -12,30 +15,55 @@ export default function TeamMember() {
                         <img className="card-img-top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQw9-Q58j8N9zKRZ_owu2OKPwPU0oxd4hx4Wg&usqp=CAU" alt="Card image cap" />
                         <div className="card-body">
                             <p className="card-text">Member</p>
+                            <div className="social-icons">
+                                <FontAwesomeIcon icon={['fab', 'fa-facebook']} />
+                                <FontAwesomeIcon icon={['fab', 'fa-instagram']} />
+                                <FontAwesomeIcon icon={['fab', 'fa-twitter']} />
+                            </div>
                         </div>
                     </div>
                     <div className="card">
                         <img className="card-img-top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQw9-Q58j8N9zKRZ_owu2OKPwPU0oxd4hx4Wg&usqp=CAU" alt="Card image cap" />
                         <div className="card-body">
                             <p className="card-text">Member</p>
+                            <div className="social-icons">
+                                <FontAwesomeIcon icon={['fab', 'fa-facebook']} />
+                                <FontAwesomeIcon icon={['fab', 'fa-instagram']} />
+                                <FontAwesomeIcon icon={['fab', 'fa-twitter']} />
+                            </div>
                         </div>
                     </div>
                     <div className="card">
                         <img className="card-img-top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQw9-Q58j8N9zKRZ_owu2OKPwPU0oxd4hx4Wg&usqp=CAU" alt="Card image cap" />
                         <div className="card-body">
                             <p className="card-text">Member</p>
+                            <div className="social-icons">
+                                <FontAwesomeIcon icon={['fab', 'fa-facebook']} />
+                                <FontAwesomeIcon icon={['fab', 'fa-instagram']} />
+                                <FontAwesomeIcon icon={['fab', 'fa-twitter']} />
+                            </div>
                         </div>
                     </div>
                     <div className="card">
                         <img className="card-img-top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQw9-Q58j8N9zKRZ_owu2OKPwPU0oxd4hx4Wg&usqp=CAU" alt="Card image cap" />
                         <div className="card-body">
                             <p className="card-text">Member</p>
+                            <div className="social-icons">
+                                <FontAwesomeIcon icon={['fab', 'fa-facebook']} />
+                                <FontAwesomeIcon icon={['fab', 'fa-instagram']} />
+                                <FontAwesomeIcon icon={['fab', 'fa-twitter']} />
+                            </div>
                         </div>
                     </div>
                     <div className="card">
                         <img className="card-img-top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQw9-Q58j8N9zKRZ_owu2OKPwPU0oxd4hx4Wg&usqp=CAU" alt="Card image cap" />
                         <div className="card-body">
                             <p className="card-text">Member</p>
+                            <div className="social-icons">
+                                <FontAwesomeIcon icon={['fab', 'fa-facebook']} />
+                                <FontAwesomeIcon icon={['fab', 'fa-instagram']} />
+                                <FontAwesomeIcon icon={['fab', 'fa-twitter']} />
+                            </div>
                         </div>
                     </div>
                 </div>
