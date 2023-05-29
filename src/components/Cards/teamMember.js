5d91a507f8ca4a99fec40ab6520f8e7d8544e0ca
AnasNemrawi <anasnemrawi3000@gmail.com>
2023-05-29 14:45:13 +0000

    adding css to the about page

--- src/components/Cards/teamMember.js
+++ src/components/Cards/teamMember.js
@@ -1,24 +1,40 @@
 import React from 'react';
 import './teamMember.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
 export default function TeamMember() {
     return (
         <div>
-            <div className="team-member-container">
-                <div className="long-text-container">
-                    <div className="long-text">
-                        <h1>
-                            Hello, we are REACTERS
-                        </h1>
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Bibendum enim facilisis gravida neque convallis  Convallis a cras semper auctor neque
-                        </p>
-                    </div>
+            <div className='AboutUs-img-div'>
+                <div className='AbouUsHeader'>
+
                 </div>
+                <div class="hero-text2   ">
+                    <h1 class="text1" >REACTERS TEAM  </h1>
+
+                </div>
+                <img className='Aboutimg' src='https://careerarc-com-wp1.s3.amazonaws.com/wp-content/uploads/2019/06/why-is-team-building-important-make-your-new-hires-feel-welcome.jpg' />
+            </div>
+            <div className="long-text-container">
+                <div className="long-text">
+                    <h1>
+                        Hello, we are REACTERS
+                    </h1>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse vel consectetur tortor. Phasellus sagittis in felis et mattis. Integer accumsan ligula ipsum, at pellentesque nisi feugiat vel. Aenean aliquet ante a fringilla dictum. In sit amet ipsum eu nunc tincidunt consequat. Mauris venenatis, erat nec gravida tempor, quam libero interdum mi, ac condimentum felis diam in velit. Quisque ac
+                    </p>
+                </div>
+            </div>
+
+            <div className="team-member-container">
+                <dev className="TeamHeader">
+                    <h1>
+                        Meet Our Team
+                    </h1>
+                </dev>
+
                 <div className="card-group-container">
                     <div className="card-group">
-                        <div className="card">
+                        <div className="card myCard">
                             <img className="card-img-top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROXGtcuYj26AD-WOGssDN3OKallo1xBIqJ9A&usqp=CAU" alt="Card image cap" />
                             <div className="card-body">
                                 <p className="card-text">Anas</p>
@@ -29,7 +45,7 @@ export default function TeamMember() {
                                 </div>
                             </div>
                         </div>
-                        <div className="card">
+                        <div className="card myCard">
                             <img className="card-img-top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROXGtcuYj26AD-WOGssDN3OKallo1xBIqJ9A&usqp=CAU" alt="Card image cap" />
                             <div className="card-body">
                                 <p className="card-text">Hasan</p>
@@ -40,7 +56,7 @@ export default function TeamMember() {
                                 </div>
                             </div>
                         </div>
-                        <div className="card">
+                        <div className="card myCard">
                             <img className="card-img-top" src="https://christophertoddstudios.com/wp-content/uploads/2021/04/ClarkFielding-8-871x1024.jpg" alt="Card image cap" />
                             <div className="card-body">
                                 <p className="card-text">Rama</p>
@@ -51,7 +67,7 @@ export default function TeamMember() {
                                 </div>
                             </div>
                         </div>
-                        <div className="card">
+                        <div className="card myCard">
                             <img className="card-img-top" src="https://christophertoddstudios.com/wp-content/uploads/2021/04/ClarkFielding-8-871x1024.jpg" alt="Card image cap" />
                             <div className="card-body">
                                 <p className="card-text">Tasneem</p>
@@ -62,7 +78,7 @@ export default function TeamMember() {
                                 </div>
                             </div>
                         </div>
-                        <div className="card">
+                        <div className="card myCard">
                             <img className="card-img-top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROXGtcuYj26AD-WOGssDN3OKallo1xBIqJ9A&usqp=CAU" alt="Card image cap" />
                             <div className="card-body">
                                 <p className="card-text">Murad</p>
