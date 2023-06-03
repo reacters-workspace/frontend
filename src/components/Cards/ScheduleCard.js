d05088c7842ef95ad776e0c5fc9fdd9b9a5a25c9
Haznto <haznto@gmail.com>
2023-06-03 20:02:37 +0000

    finalsteps adding one more page

---
+++ src/components/Cards/ScheduleCard.js
@@ -0,0 +1,53 @@
+import axios from 'axios';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import "../Cards/ScheduleCard.css"
+import { useState } from 'react';
+
+function ScheduleCard({ singleCardData, deleteChangeState, handleShow, setsingleExercise, setOnUpdate, setisUpdated }) {
+
+    function onShowClick() {
+        setOnUpdate(false)
+        setsingleExercise(singleCardData)
+        handleShow()
+    }
+
+    function ChangeStateToUpdate() {
+        setsingleExercise(singleCardData)
+        setOnUpdate(true)
+        handleShow()
+        setisUpdated(false)
+    }
+    function updateScheduleDay() {
+        const updatedDay = {
+            week_day: ""
+        }
+        axios.put(`http://localhost:3009/update-schedule/${singleCardData.id}`, updatedDay)
+
+    }
+    function deleteSchedule() {
+        axios.delete(`http://localhost:3009/deleteSchedule/${singleCardData.id}`).then(returned => console.log(returned))
+            .catch(err => console.log(err))
+
+        deleteChangeState(singleCardData.id)
+    }
+    return (
+        <Card className='allCard' style={{ width: '18rem', margin: "0" }}>
+            <Card.Img className="image1" variant="top" src={singleCardData.gifurl} />
+            <Card.Body className='card-body-schedule'>
+                <Card.Text className="title1">{singleCardData.bodypart}</Card.Text>
+                <Card.Title className="title1">
+                    {singleCardData.exercise_name}
+                </Card.Title>
+                <Button className="show-btn w-50"
+                    variant="btn btn-dark" onClick={onShowClick}>Show</Button>
+                <div className='schedule-buttons'>
+                    <Button className="change-button" onClick={ChangeStateToUpdate} variant="secondary">Change Day</Button>
+                    <Button variant="danger" onClick={deleteSchedule}>Delete</Button>
+                </div>
+            </Card.Body>
+        </Card>
+    );
+}
+
+export default ScheduleCard;
---
+++ src/components/SchedulePage/SchedulePage.js
@@ -0,0 +1,138 @@
+import { Button, Card, Row } from "react-bootstrap"
+import ExerciseCard1 from "../Cards/CardExerciseGif"
+import "../SchedulePage/SchedulePage.css"
+import { useEffect, useState } from "react";
+import ModalExercise from "../ModalExercise/ModalExercise";
+import axios from "axios";
+import ScheduleCard from "../Cards/ScheduleCard";
+export default function SchedulePage({ onSchedulePage }) {
+    const [singleExercise, setsingleExercise] = useState({});
+    const [scheduleCards, setScheduleCards] = useState([]);
+    const [onUpdate, setOnUpdate] = useState(false)
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+    const [isUpdated, setisUpdated] = useState(false)
+
+    const [exerciseData, setExerciseData] = useState([]);
+    function handleOnClick() {
+        handleShow();
+
+    }
+    function fetchData() {
+        axios
+            .get(`http://localhost:3009/schedule`)
+            .then((result) => {
+                console.log(result.data.data)
+                const exerciseDB = result.data.data;
+
+                setScheduleCards(exerciseDB);
+            });
+    }
+    function deleteChangeState(id) {
+        setScheduleCards(beforeDelete => beforeDelete.filter(Schedulefiltered => Schedulefiltered.id !== id))
+    }
+    useEffect(() => {
+        fetchData();
+    }, [isUpdated]);
+
+
+
+    return (
+        <>
+            <div className="parent-schedule-div">
+                <div className="days-div">
+
+                    <Row className="cards-in-schedule-page">
+                        <h2>Sunday</h2>
+                        {scheduleCards.map(item => {
+                            if (item.week_day === 'sunday')
+                                return (
+
+                                    <ScheduleCard setisUpdated={setisUpdated} setOnUpdate={setOnUpdate} setsingleExercise={setsingleExercise} handleShow={handleShow} singleCardData={item} key={item.id} deleteChangeState={deleteChangeState} />
+
+                                )
+
+                        })}
+
+
+
+                        <h2>Monday</h2>
+                        {scheduleCards.map(item => {
+                            if (item.week_day === 'monday')
+                                return (
+
+                                    <ScheduleCard setisUpdated={setisUpdated} setOnUpdate={setOnUpdate} setsingleExercise={setsingleExercise} handleShow={handleShow} singleCardData={item} key={item.id} deleteChangeState={deleteChangeState} />
+                                )
+
+                        })}
+
+
+                        <h2>Tuesday</h2>
+                        {scheduleCards.map(item => {
+                            if (item.week_day === 'tuesday')
+                                return (
+
+                                    <ScheduleCard setisUpdated={setisUpdated} setOnUpdate={setOnUpdate} setsingleExercise={setsingleExercise} handleShow={handleShow} singleCardData={item} key={item.id} deleteChangeState={deleteChangeState} />
+                                )
+
+                        })}
+
+
+                        <h2>Wednesday</h2>
+                        {scheduleCards.map(item => {
+                            if (item.week_day === 'wednesday')
+                                return (
+
+                                    <ScheduleCard setisUpdated={setisUpdated} setOnUpdate={setOnUpdate} setsingleExercise={setsingleExercise} handleShow={handleShow} singleCardData={item} key={item.id} deleteChangeState={deleteChangeState} />
+                                )
+
+                        })}
+
+
+
+                        <h2>Thursday</h2>
+                        {scheduleCards.map(item => {
+                            if (item.week_day === 'thursday')
+                                return (
+
+                                    <ScheduleCard setisUpdated={setisUpdated} setOnUpdate={setOnUpdate} setsingleExercise={setsingleExercise} handleShow={handleShow} singleCardData={item} key={item.id} deleteChangeState={deleteChangeState} />
+                                )
+
+                        })}
+
+
+                        <h2>Friday</h2>
+                        {scheduleCards.map(item => {
+                            if (item.week_day === 'friday')
+                                return (
+
+                                    <ScheduleCard setisUpdated={setisUpdated} setOnUpdate={setOnUpdate} setsingleExercise={setsingleExercise} handleShow={handleShow} singleCardData={item} key={item.id} deleteChangeState={deleteChangeState} />
+                                )
+
+                        })}
+
+
+                        <h2>Saturday</h2>
+                        {scheduleCards.map(item => {
+                            if (item.week_day === 'saturday')
+                                return (
+
+                                    <ScheduleCard setisUpdated={setisUpdated} setOnUpdate={setOnUpdate} setsingleExercise={setsingleExercise} handleShow={handleShow} singleCardData={item} key={item.id} deleteChangeState={deleteChangeState} />
+                                )
+
+                        })}
+                    </Row>
+                </div>
+                <ModalExercise
+                    onUpdate={onUpdate}
+                    show={show}
+                    handleClose={handleClose}
+                    singleExercise={singleExercise}
+                    onSchedulePage={onSchedulePage}
+                    setisUpdated={setisUpdated}
+                />
+            </div >
+        </>
+    )
+}
